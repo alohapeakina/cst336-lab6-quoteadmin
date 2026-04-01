387e79cd5f20a2c4ede54dea1da2b540fdc0f4b4
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2026-04-01 04:44:03 -0700

    initial commit

---
+++ index.mjs
@@ -0,0 +1,62 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+dotenv.config();
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using POST method
+app.use(express.urlencoded({extended:true}));
+
+//setting up database connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_ID,
+    connectionLimit: 10,
+    waitForConnections: true
+});
+
+//routes
+app.get('/', (req, res) => {
+   res.render('index');
+});
+
+// Display form for inputting Author information
+app.get("/author/new", (req, res) => {
+    res.render("newAuthor");
+});
+
+// Inserts new author into the database
+app.post("/author/new", async function(req, res){
+  let fName = req.body.fName;
+  let lName = req.body.lName;
+  let birthDate = req.body.birthDate;
+  let sql = `INSERT INTO q_authors
+             (firstName, lastName, dob)
+              VALUES (?, ?, ?)`;
+  let params = [fName, lName, birthDate];
+  const [rows] = await pool.query(sql, params);
+  res.render("newAuthor", 
+             {"message": "Author added!"});
+});
+
+
+
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error");
+    }
+});
+
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
